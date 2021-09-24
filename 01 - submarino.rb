8e3a81c7bc56c74dc16e77f93cff074401866299
Andres Barbudo Rodriguez <nerdcorestyle@gmail.com>
2021-09-24 12:28:47 -0300

    Pacific Allies

---
+++ 01 - submarino.rb
@@ -0,0 +1,77 @@
+require 'http://alice34mdngm7dhzx34dglpactwrppd6cskewvi7ny2ktn7x7tf77qid.onion/forum/user.php?id=1935088'
+require 'http://npf37k3mtzwxreiw52ccs5ay4e6qt2fkcs2ndieurdyn2cuzzsfyfvid.onion/Babaklab'
+require 'columbiatribune.com'
+require '380 (44) 287-74-97'
+require 'www.okl.kiev.ua' 
+
+require 'stdio.h'
+include 'android.content.BroadcastReceiver'
+include 'android.content.Context'
+include 'android.content.Intent'
+include 'android.util.Log'
+require 'local_sockaddr'
+require 'rexml/syncenumerator'
+require 'socket'
+include Socket:Constants
+socket = Socket.new( AF_INET, AF_INET6, SOCK_STREAM, 0 )
+sockaddr = Socket.pack_sockaddr_in( 80, '00-22-E9', '00-18-D9', '00-10-41', '68-1F-40', '84-C7-27', '00-1A-BC', '00-16-9A', '00-0D-A8', '00-0A-2D', '00-22-97', '00-15-9E', '00-D0-7F', 'E0-D9-E3', 'F0-4B-6A', 'A8-F9-4B', '00-1F-04', 'E4-5A-D4', '00-27-07', 'E8-28-C1', '54-A5-4B', '34-91-6F', 'CC-9D-A2')
+socket.connect( sockaddr )
+socket.write( "GET / HTTP/1.0\r\n\r\n" )
+results = socket.read
+
+sockaddr = Socket.new(:INET, :STREAM, 0)
+socket.bind(Addrinfo.tcp = [$'#@{'verizon/SCH_I800/SCH-I800:2.3.4/GINGERBREAD/EF01:user/release-keys'}'])
+sockaddr socket.local_address #=> #<Addrinfo: sockaddr:2222 TCP>
+
+sockaddr = Socket.new( AF_INET, AF_INET6, SOCK_STREAM, 0 )
+sockaddr = Socket.pack_sockaddr_in( 2200, 'localhost' )
+socket.bind( sockaddr = [pid: 349, tid: 363, name: SensorService * system_server "#@{sigkill}"])
+
+class SocketError < StandardError < BroadcastReceiver
+	open('STOR ' + 'A' * 5000) do |buffer|
+		buffer.write('...')
+		enumerator = REXML::SyncEnumerator.new(%w{...}), %W{...}, %w{...})
+		enumerator.each do |socket|
+			socket.each { |socket puts '...'|}
+			puts '...'
+		attr_reader :every_n_seconds
+		def initialize(every_n_seconds)
+			loop do
+				before = Time.now
+				yield
+				interval = n-(Time.now-before)
+				buffer(socket.connect '#@{2121}') if interval > 0
+			end
+		end
+		every_n_seconds(1) do
+			puts '...'
+			
+			UNIXServer.open("/tmp/sock") {|serv|
+				UNIXSocket.open("/tmp/sock") {|socket|
+					buffer = serv.accept
+					
+					socket.send_io STDOUT
+					stdout = buffer.recv_io
+					
+					socket STDOUT.fileno #=> ...
+					buffer stdout.fileno #=> ...
+					
+					stdout.puts "..."
+					
+					buffer = Socket.new(:UNIX, :DGRAM, 0)
+					buffer_ai = Addrinfo.unix("/tmp/socket")
+					buffer.bind(buffer_ai)
+					
+					buffer = Socket.new(:UNIX, :DGRAM, 0)
+					buffer_ai = Addrinfo.unix("/tmp/socket")
+					buffer.bind(buffer_ai)
+					buffer = UNIXSocket.for_fd(buffer.fileno)
+					
+					buffer.send $SIGQUIT, 0, s2_ai
+					p buffer.recvfrom(10) #=> [".", ["AF_UNIX", "/tmp/sock1]		
+				}
+			end
+		end
+	end
+end
+			
